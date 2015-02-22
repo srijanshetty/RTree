ac3ae28d92b54b05339c1fbf08a950be334c0d3b
Srijan R Shetty <srijan.shetty@gmail.com>
2015-02-22 14:34:04 +0530

    storeNodeToDisk

--- rtree.cpp
+++ rtree.cpp
@@ -91,6 +91,109 @@ namespace RTree {
             // Read the node from the disk
             void readNodeFromDisk();
     };
+
+    // The root of the tree
+    Node *Rroot = nullptr;
+
+    // Initial value of the nodeCount
+    long Node::nodeCount = 0;
+    long Node::lowerBound = 0;
+    long Node::upperBound = 0;
+
+    Node::Node() {
+        // Assign a fileIndex to the current node
+        fileIndex = ++nodeCount;
+    }
+
+    Node::Node(long _fileIndex) {
+        // Assign the given fileIndex to the node
+        fileIndex = _fileIndex;
+
+        // Load the node from the disk
+        readNodeFromDisk();
+    }
+
+    void Node::initialize() {
+        // Save some place in the file for the header
+        long headerSize =
+            sizeof(leaf)
+            + sizeof(fileIndex)
+            + sizeof(parentIndex)
+            + sizeof(sizeOfSubtree);
+        long pageSize = PAGESIZE - headerSize;
+        long keySize = sizeof(childIndices.front());
+        long nodeSize = sizeof(fileIndex);
+
+        // Compute the bounds
+        upperBound = pageSize / (2 * DIMENSION * keySize + nodeSize);
+        lowerBound = upperBound / 2;
+
+        // TODO: clear this up
+        upperBound = 10;
+        lowerBound = 5;
+    }
+
+    void Node::storeNodeToDisk() {
+        char buffer[PAGESIZE];
+        long location = 0;
+
+        // Store the contents to disk
+        memcpy(buffer + location, &leaf, sizeof(leaf));
+        location += sizeof(leaf);
+
+        memcpy(buffer + location, &fileIndex, sizeof(fileIndex));
+        location +=sizeof(fileIndex);
+
+        memcpy(buffer + location, &parentIndex, sizeof(parentIndex));
+        location += sizeof(parentIndex);
+
+        memcpy(buffer + location, &sizeOfSubtree, sizeof(sizeOfSubtree));
+        location += sizeof(sizeOfSubtree);
+
+        // Add the bounds of the MBR to the tree
+        for (auto upperPoint : upperPoints) {
+            memcpy(buffer + location, &upperPoint, sizeof(upperPoint));
+            location += sizeof(upperPoint);
+        }
+
+        for (auto lowerPoint : lowerPoints) {
+            memcpy(buffer + location, &lowerPoint, sizeof(lowerPoint));
+            location += sizeof(lowerPoint);
+        }
+
+        // For the leaf we store objectPointers, else we store children
+        if (!leaf) {
+            // We will have to store the number of children for each node
+            long numberOfChildren = childIndices.size();
+            memcpy(buffer + location, &numberOfChildren, sizeof(numberOfChildren));
+            location += sizeof(numberOfChildren);
+
+            // Now we store the children of the current element
+            for (auto childIndex : childIndices) {
+                memcpy(buffer + location, &childIndex, sizeof(childIndex));
+                location += sizeof(childIndex);
+            }
+        } else {
+            // We will have to store the number of objects for each leaf
+            long numberOfObjects = objectIndices.size();
+            memcpy(buffer + location, &numberOfObjects, sizeof(numberOfObjects));
+            location += sizeof(numberOfObjects);
+
+            // Now we store the children of the current element
+            for (auto objectIndex : objectIndices) {
+                memcpy(buffer + location, &objectIndex, sizeof(objectIndex));
+                location += sizeof(objectIndex);
+            }
+        }
+
+        // Now we copy the buffer to disk
+        ofstream ofile(getFileName(), ios::binary | ios::out);
+        ofile.write(buffer, PAGESIZE);
+        ofile.close();
+    }
+
+    void Node::readNodeFromDisk() {
+    }
 };
 
 int main() {
